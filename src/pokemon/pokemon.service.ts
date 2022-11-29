02693b1253ca7066a06825fec59935d7d0624cbf
Esteban <juanescalderon12@gmail.com>
2022-11-29 17:37:25 -0500

    Find By in PokeAPI already

--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -1,6 +1,7 @@
-import { BadRequestException, Injectable, InternalServerErrorException } from '@nestjs/common';
+import { BadRequestException, Injectable, InternalServerErrorException, NotFoundException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import { Model } from 'mongoose';
+import { IsMongoId } from 'class-validator';
+import { isValidObjectId, Model } from 'mongoose';
 import { CreatePokemonDto } from './dto/create-pokemon.dto';
 import { UpdatePokemonDto } from './dto/update-pokemon.dto';
 import { Pokemon } from './entities/pokemon.entity';
@@ -34,8 +35,24 @@ export class PokemonService {
     return `This action returns all pokemon`;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} pokemon`;
+  async findOne(term: string) {
+
+    let pokemon: Pokemon;
+
+    // Verificación por number
+    // Aquí no recibimos números. El condicional valida si el término de búsqueda es un número.
+    if( !isNaN(+term) ) pokemon = await this.pokemonModel.findOne({ number: term });
+
+    //Verificación por MongoID
+    if(!pokemon && isValidObjectId(term)) pokemon = await this.pokemonModel.findById(term);
+
+    //Verificación por name
+    if(!pokemon) pokemon = await this.pokemonModel.findOne({ name: term.toLowerCase().trim() });
+
+    // Si no cumple niguna condición, se retorna un 404.
+    if(!pokemon) throw new NotFoundException(`Pokemon with id, name or number ${term} not found`);
+
+    return pokemon;
   }
 
   update(id: number, updatePokemonDto: UpdatePokemonDto) {
