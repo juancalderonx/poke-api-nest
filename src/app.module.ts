0e76d0e5d01289ab092103e4b8bb021dabe09435
Esteban <juanescalderon12@gmail.com>
2022-12-01 12:04:54 -0500

    Adding my firts env variables :)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,18 +1,24 @@
 import { join } from 'path';
+
 import { Module } from '@nestjs/common';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
+
 import { PokemonModule } from './pokemon/pokemon.module';
 import { CommonModule } from './common/common.module';
 import { SeedModule } from './seed/seed.module';
 
 @Module({
   imports: [
+
+    ConfigModule.forRoot(),
+
     ServeStaticModule.forRoot({
     rootPath: join(__dirname,'..','public'),
     }),
 
-    MongooseModule.forRoot('mongodb://localhost:27017/nest-pokemon'),
+    MongooseModule.forRoot(process.env.MONGODB),
 
     PokemonModule,
 
@@ -23,4 +29,10 @@ import { SeedModule } from './seed/seed.module';
   controllers: [],
   providers: [],
 })
-export class AppModule {}
+export class AppModule {
+
+  constructor() {
+    console.log(process.env);
+  }
+
+}
